73b60298c739f6269cd01cd35317c5514c313fec
ladria <adria.ameijide@gmail.com>
2015-11-13 00:07:54 +0100

    1a versió

---
+++ Prèstec de Portàtils UPF/Prèstec de Portàtils UPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Prèstec de Portàtils UPF")]
+[assembly: AssemblyDescription("Ets estudiant de la Universitat Pompeu Fabra? Vols agafar un portàtil de prèstec però no saps si hi ha cua? Amb aquesta app tindràs tota aquesta informació a l'abast!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("adriapps")]
+[assembly: AssemblyProduct("Prèstec de Portàtils UPF")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("adriapps")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en False, los componentes COM no verán los 
+// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en True en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("bd8e4fbd-efd2-4afb-ae17-20ddb953f341")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y revisión 
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ca-ES")]
